99e67c6568eada187ab32e82f8a8a9657a521120
Rich Sposato <git_hub@richsposato.com>
2018-03-04 19:30:08 -0800

    Added echo statements to show build progress.

--- src/make_it.sh
+++ src/make_it.sh
@@ -5,13 +5,13 @@ if [ ! -d "obj" ]; then
 	mkdir obj
 fi
 
-g++ -std=c++14 -Wall -I../include -c AllocatorManager.cpp    -o ./obj/AllocatorManager.o
-g++ -std=c++14 -Wall -I../include -c TinyBlock.cpp           -o ./obj/TinyBlock.o
-g++ -std=c++14 -Wall -I../include -c PoolBlock.cpp           -o ./obj/PoolBlock.o
-g++ -std=c++14 -Wall -I../include -c StackBlock.cpp          -o ./obj/StackBlock.o
-g++ -std=c++14 -Wall -I../include -c LinearBlock.cpp         -o ./obj/LinearBlock.o
-g++ -std=c++14 -Wall -I../include -c PoolAllocator.cpp       -o ./obj/PoolAllocator.o
-g++ -std=c++14 -Wall -I../include -c StackAllocator.cpp      -o ./obj/StackAllocator.o
-g++ -std=c++14 -Wall -I../include -c LinearAllocator.cpp     -o ./obj/LinearAllocator.o
-g++ -std=c++14 -Wall -I../include -c TinyObjectAllocator.cpp -o ./obj/TinyObjectAllocator.o
-
+echo "Compile AllocatorManager.cpp";    g++ -std=c++14 -Wall -I../include -c AllocatorManager.cpp    -o ./obj/AllocatorManager.o
+echo "Compile TinyBlock.cpp";           g++ -std=c++14 -Wall -I../include -c TinyBlock.cpp           -o ./obj/TinyBlock.o
+echo "Compile PoolBlock.cpp";           g++ -std=c++14 -Wall -I../include -c PoolBlock.cpp           -o ./obj/PoolBlock.o
+echo "Compile StackBlock.cpp";          g++ -std=c++14 -Wall -I../include -c StackBlock.cpp          -o ./obj/StackBlock.o
+echo "Compile LinearBlock.cpp";         g++ -std=c++14 -Wall -I../include -c LinearBlock.cpp         -o ./obj/LinearBlock.o
+echo "Compile PoolAllocator.cpp";       g++ -std=c++14 -Wall -I../include -c PoolAllocator.cpp       -o ./obj/PoolAllocator.o
+echo "Compile StackAllocator.cpp";      g++ -std=c++14 -Wall -I../include -c StackAllocator.cpp      -o ./obj/StackAllocator.o
+echo "Compile LinearAllocator.cpp";     g++ -std=c++14 -Wall -I../include -c LinearAllocator.cpp     -o ./obj/LinearAllocator.o
+echo "Compile TinyObjectAllocator.cpp"; g++ -std=c++14 -Wall -I../include -c TinyObjectAllocator.cpp -o ./obj/TinyObjectAllocator.o
+echo "Done!"
--- test/functional/make_it.sh
+++ test/functional/make_it.sh
@@ -1,15 +1,18 @@
 
 rm functionality_test.exe
 
-g++ -std=c++14 -Wall -I../../include -I../../../Hestia/CppUnitTest/include -c main.cpp -o main.o
-g++ -std=c++14 -Wall -I../../include -I../../../Hestia/CppUnitTest/include -c TestTinyBlock.cpp -o TestTinyBlock.o
-g++ -std=c++14 -Wall -I../../include -I../../../Hestia/CppUnitTest/include -c TestPoolBlock.cpp -o TestPoolBlock.o
-g++ -std=c++14 -Wall -I../../include -I../../../Hestia/CppUnitTest/include -c TestStackBlock.cpp -o TestStackBlock.o
-g++ -std=c++14 -Wall -I../../include -I../../../Hestia/CppUnitTest/include -c TestLinearBlock.cpp -o TestLinearBlock.o
-g++ -std=c++14 -Wall -I../../include -I../../../Hestia/CppUnitTest/include -c CommandLineArgs.cpp -o CommandLineArgs.o
+echo "Compile main.cpp";            g++ -std=c++14 -Wall -I../../include -I../../../Hestia/CppUnitTest/include -c main.cpp -o main.o
+echo "Compile ChunkList.cpp";       g++ -std=c++14 -Wall -o ChunkList.o -c ChunkList.cpp
+echo "Compile TestTinyBlock.cpp";   g++ -std=c++14 -Wall -I../../include -I../../../Hestia/CppUnitTest/include -c TestTinyBlock.cpp -o TestTinyBlock.o
+echo "Compile TestPoolBlock.cpp";   g++ -std=c++14 -Wall -I../../include -I../../../Hestia/CppUnitTest/include -c TestPoolBlock.cpp -o TestPoolBlock.o
+echo "Compile TestStackBlock.cpp";  g++ -std=c++14 -Wall -I../../include -I../../../Hestia/CppUnitTest/include -c TestStackBlock.cpp -o TestStackBlock.o
+echo "Compile TestLinearBlock.cpp"; g++ -std=c++14 -Wall -I../../include -I../../../Hestia/CppUnitTest/include -c TestLinearBlock.cpp -o TestLinearBlock.o
+echo "Compile CommandLineArgs.cpp"; g++ -std=c++14 -Wall -I../../include -I../../../Hestia/CppUnitTest/include -c CommandLineArgs.cpp -o CommandLineArgs.o
 
+echo "Linking"
 g++ -std=c++14 -Wall -o functionality_test.exe \
 	main.o \
+	ChunkList.o \
 	TestTinyBlock.o \
 	TestPoolBlock.o \
 	TestStackBlock.o \
@@ -25,3 +28,4 @@ g++ -std=c++14 -Wall -o functionality_test.exe \
 	../../src/obj/LinearAllocator.o \
 	../../src/obj/TinyObjectAllocator.o \
 	../../../Hestia/CppUnitTest/src/obj/UnitTest.o
+echo "Done!"
